1edef834afc646067c9a97b645cb425a319414dc
Fiifi Ofori <fiifi56@gmail.com>
2021-06-23 02:07:07 +0000

    html changed

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+// $(document).ready(function(){
+
+//     $("a.add").on("click",function(){
+
+//         // console.log('working');
+
+//     $("div:last").after("<div class='card p-4'><h4>App</h4><p>App id: 1234567890</p><button type='button' class='btn btn-dark butt mt-4'>Open</button></div>");
+
+//     })
+
+// })
+
+$('#insert').click(function(){
+
+    $( "ul#list" ).append( "<li>any html here</li>" );
+
+});

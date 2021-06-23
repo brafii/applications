7c1c7925fc9eac01aed66b2858b1525534d4ecc7
Fiifi Ofori <fiifi56@gmail.com>
2021-06-23 02:18:08 +0000

    js logic to add cards working

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,6 @@
 
 $('#insert').click(function(){
 
-    $( "ul#list" ).append( "<li>any html here</li>" );
+    $( "ul#list" ).append( "<li class='col-md-6 mb-2' id='element2'><div class='card p-4'><h4>App</h4><p>App id: 1234567890</p><button type='button' class='btn btn-dark butt mt-4'>Open</button></div></li>" );
 
 });

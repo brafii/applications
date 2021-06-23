4894fb3b253b8add786360f97fd4729e3e978d98
Fiifi Ofori <fiifi56@gmail.com>
2021-06-23 14:18:19 +0000

    solving js issue to display array of info

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 //Custom object to repesent my API
-function api(name, id) {
-    this.name = name;
-    this.maker = maker;
-}
 
-let api1 = new API('App', '2222hhbbfhf@$22');
-let api2 = new API('SHACK15 App', '2222hh000hf@$22');
+var apis = [ 
+    { 
+      appID: ['02300XSS000ASss','22300XSS000ASss','22300XSS000ASss'],
+      userName : 'App',
+    },
+    // { 
+    //   appID: 'zz300XSS000ASss',
+    //   userName : 'SHACK15 App',
+    // },
+  ];  
+
+// for(let api of apis){
+//     console.log(api.appID[0]);
+// }
+
 
 
 // event handler to dynamically add new containers
 document.querySelector('#insert').addEventListener('click', function(){
 
-    $( "ul#list" ).append( "<li class='col-md-6 mb-2' id='element2'><div class='card p-4'><h4>App</h4><p>App id: 1234567890</p><button type='button' class='btn btn-dark butt mt-4'>Open</button></div></li>" );
+    for (let api of apis) {
+        console.log(api.appID[0]);
+        $( "ul#list" ).append('<li class="col-md-6 mb-2" id="element2"><div class="card p-4"><h4>App</h4><p>App id: 1234567890</p><button type="button" class="btn btn-dark butt mt-4">Open</button></div></li>');
+    }
  
 });

11d9406a6fc725a8b493c451f029a3be48f02fb6
Fiifi Ofori <fiifi56@gmail.com>
2021-06-23 02:23:02 +0000

    js logic to add new cards working

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,7 @@
 
-// $(document).ready(function(){
-
-//     $("a.add").on("click",function(){
-
-//         // console.log('working');
-
-//     $("div:last").after("<div class='card p-4'><h4>App</h4><p>App id: 1234567890</p><button type='button' class='btn btn-dark butt mt-4'>Open</button></div>");
-
-//     })
-
-// })
-
-$('#insert').click(function(){
+// event handler to dynamically add new containers
+document.querySelector('#insert').addEventListener('click', function(){
 
     $( "ul#list" ).append( "<li class='col-md-6 mb-2' id='element2'><div class='card p-4'><h4>App</h4><p>App id: 1234567890</p><button type='button' class='btn btn-dark butt mt-4'>Open</button></div></li>" );
-
+ 
 });
